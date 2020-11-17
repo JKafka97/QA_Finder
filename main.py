4805f1767b6a357aae34f6adeec95a4194642089
JKafak97 <dr.jara1@seznam.cz>
2020-11-17 14:50:16 +0100

    I make first structure

--- main.py
+++ main.py
@@ -1,9 +1,44 @@
 import sys
-import pdftotext
 from pathlib import Path
 
 
 def main():
+    pass
+
+
+def get_source_from_csv():
+    pass
+
+
+def clean_source_data():
+    pass
+
+
+def show_source_to_user():
+    pass
+
+
+def approve_source():
+    pass
+
+
+def get_target_from_pdf():
+    pass
+
+
+def get_pdf_name():
+    pass
+
+
+def make_clean_text():
+    pass
+
+
+def source_and_target_compare():
+    pass
+
+
+def compare_result():
     pass
 
 
